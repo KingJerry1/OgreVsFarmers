653269cbb2139922fc818c2f2881f2d2196cdb77
KingJerry1 <malyjurek.b@gmial.com>
2019-12-08 22:17:45 +0100

    Animations, spawning attackers and defenders

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void TriggerDeathVFX() {
+        if(!deathVFX) { return; }
+        GameObject deathVFXObject = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathVFXObject, 1f);
+    }
+
+    public void DealDamage(float damage) {
+        health -= damage;
+        if(health <= 0) {
+
+            TriggerDeathVFX();
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
